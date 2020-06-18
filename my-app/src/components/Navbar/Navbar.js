3d4df2708035d0176ae61591e7f792d53ea352e2
Nora Lashner <nlashner@gmail.com>
2020-06-18 12:43:43 -0400

    updated font and margins on navbar

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 //import {Link} from 'react-router-dom'
+import logo from './Logo_Black.png'
 import './navbar.css'
 
 const Navbar = () => {
   return (
   <div className="navbar">
+
     <div className="left">
+    <img src={logo} className='nav-logo'></img>
     <p>nlashner@gmail.com</p>
     </div>
+
     <div className="right">
-    <p>My Projects</p>
-    <p>Experience</p>
-    <p>About Me</p>
+    <p className='right-item'>My Projects</p>
+    <p className='right-item'>Experience</p>
+    <p className='right-item'>About Me</p>
     </div>
   </div>
   )
