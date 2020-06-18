d4a0cc3329cc674bc9b7cbf8e80eb62cd6cc15f0
Nora Lashner <nlashner@gmail.com>
2020-06-18 16:15:52 -0400

    added to the about me section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Intro from './components/Intro/Intro'
 import Projects from './components/Projects/Projects'
 import Experience from './components/Experince/Experience'
+import AboutMe from './components/AboutMe/AboutMe'
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Intro />
       <Projects />
       <Experience />
+      <AboutMe />
     </div>
   );
 }
---
+++ my-app/src/components/AboutMe/AboutMe.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import picture from './IMG_0561.png'
+import './aboutMe.css'
+
+const AboutMe = () => {
+  return (
+    <div className="aboutMe-conatiner">
+
+      <div className='aboutMe-info'>
+      <img src={picture} className="photo" alt="nora-photo"></img>
+      <div className="title">About Me</div>
+      <div className='aboutMe-info'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+      </div>
+      </div>
+
+      <div className='spotify-iframe'>
+      <iframe src="https://open.spotify.com/embed/playlist/60c0vcILpnnUCf33lTUe8u" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
+
+    </div>
+  )
+}
+
+export default AboutMe
