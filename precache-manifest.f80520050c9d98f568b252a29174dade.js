747e47c569cd0a1c170024b6d0a0cbde8e2f14b0
Nora Lashner <nlashner@gmail.com>
2020-07-21 11:08:53 -0400

    Updates

---
+++ precache-manifest.f80520050c9d98f568b252a29174dade.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c97aac8504b0cfb5b540e3885d69fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fac2db900dc4cc4928e",
+    "url": "/static/css/main.8624956f.chunk.css"
+  },
+  {
+    "revision": "08cae21cd2bda60bfdd5",
+    "url": "/static/js/2.648c3eee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.648c3eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fac2db900dc4cc4928e",
+    "url": "/static/js/main.c1580dda.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "08a905522138c5dc315969bd034f8022",
+    "url": "/static/media/AngelistB.08a90552.png"
+  },
+  {
+    "revision": "6cc1a5088d27e19bd0101a73172d2b8d",
+    "url": "/static/media/GithubB.6cc1a508.png"
+  },
+  {
+    "revision": "17a0ccd9ee5535db6d9bb7751772b00c",
+    "url": "/static/media/IMG_0561.17a0ccd9.png"
+  },
+  {
+    "revision": "538f5d989180f57d972c68f8e9cf9e39",
+    "url": "/static/media/LinkedInB.538f5d98.png"
+  },
+  {
+    "revision": "54050dc504e9cfeddb48e067555104e6",
+    "url": "/static/media/Logo_Black.54050dc5.png"
+  },
+  {
+    "revision": "7a89ac8ac0b9be253610301beae86f09",
+    "url": "/static/media/Logo_KO.7a89ac8a.png"
+  },
+  {
+    "revision": "4ce7b3187cd4c318feff3974bdbc5dcd",
+    "url": "/static/media/TwitterB.4ce7b318.png"
+  },
+  {
+    "revision": "6b29451c8ae2c74a59b711fd89a03e97",
+    "url": "/static/media/chefRemy.6b29451c.png"
+  },
+  {
+    "revision": "484f428f001e60d3ab5dd723ae9b87b0",
+    "url": "/static/media/diagonElley.484f428f.png"
+  },
+  {
+    "revision": "e61fe6301d028e84b2bbebf9b84a7836",
+    "url": "/static/media/queueApp.e61fe630.png"
+  }
+]);
